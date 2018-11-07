a6c1a8cc22beac75a6530a43dbef8b4b99f8d685
Adrián <adri4silva@gmail.com>
2018-11-08 00:42:19 +0100

    Add extensions

---
+++ MatrixPedia/Extensions/UIApplication+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  UIApplication+Extensions.swift
+//  MatrixPedia
+//
+//  Created by Adrián Silva on 07/11/2018.
+//  Copyright © 2018 Adrián Silva. All rights reserved.
+//
+
+import UIKit
+
+extension UIApplication {
+    static var serviceLocator: ServiceLocator = ServiceLocator()
+}
